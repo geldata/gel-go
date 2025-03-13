8193180863478cc87cc81ff21039cebb0eb9e6f2
fmoor <fmoor@gmx.com>
2025-03-13 10:57:59 -0700

    Compile *ALL* edgeql-go output in tests

--- cmd/edgeql-go/testdata/pubtypes/test-project2/ignore.go
+++ cmd/edgeql-go/testdata/pubtypes/test-project2/ignore.go
@@ -3,6 +3,13 @@ package main
 import (
 	"context"
 
+	_ "test/argnames"
+	_ "test/object"
+	_ "test/scalar"
+	_ "test/tuple"
+	_ "test/when_no_go_files_in_dir_dir_name_becomes_package_name"
+	_ "test/when_no_go_files_in_dir_dir_name_becomes_package_name/subpkg"
+
 	"github.com/geldata/gel-go"
 	"github.com/geldata/gel-go/gelcfg"
 )
